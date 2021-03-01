905349f6a524ef1943ef70b95cbc184a5eb233fc
Swarup Kharul <swarupkharulsk@gmail.com>
2021-03-01 12:55:22 +0530

    added static url

--- login_sighnup_email/settings.py
+++ login_sighnup_email/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,3 +122,4 @@ USE_TZ = True
 AUTH_USER_MODEL = 'authentication.MyUser'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
